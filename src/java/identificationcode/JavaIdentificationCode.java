0a5ff21ad8efdd251375cf8cd48e0e90fc26a841
Matthew Larrubia <mlarrubia@columbushs.com>
2020-10-28 10:40:19 -0400

    key

--- src/java/identificationcode/JavaIdentificationCode.java
+++ src/java/identificationcode/JavaIdentificationCode.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package java.identificationcode;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +17,59 @@ public class JavaIdentificationCode {
      */
     public static void main(String[] args) {
         // Happy Coding!
+
+        // substring
+        // indexOf
+        // lastIndexOf
+        // length
+        // toUpperCase
+        // concatenate
+
+        // Create Scanner object
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("PLease enter your location: ");
+        String location = input.nextLine();
+
+        System.out.println("Please enter your DOB");
+        String dob = input.nextLine();
+
+        System.out.println("Please Enter your Gender: ");
+        String gender = input.nextLine();
+
+        // First Letter of First Name
+        String firstLetterFirstname = name.substring(0, 1);
+
+        int indexOfSpace = name.indexOf(" ");
+        int beginningIndex = indexOfSpace + 1;
+        int endingIndex = indexOfSpace + 2;
+
+        String firstLetterLastName = name.substring(beginningIndex, endingIndex);
+
+        String hyphen = "-";
+
+        // Last 3 of zip code
+        String zipcode = location.substring(location.length() - 3);
+        // last 2 of DOB
+        String yearDOB = dob.substring(dob.length() - 2);
+
+        // First letter of gender
+        String genderLetter = gender.substring(0, 1);
+
+        // State
+        int indexOfComma = location.indexOf(", ");
+        String state = location.substring(indexOfComma + 2, indexOfComma + 4);
+
+        String id = firstLetterFirstname + firstLetterLastName + hyphen + zipcode + yearDOB + hyphen + genderLetter
+                + state;
+
+        String finalID = id.toUpperCase();
+
+        System.out.println("Code: " + finalID);
+
     }
-    
+
 }
